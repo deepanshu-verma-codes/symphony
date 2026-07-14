ff9145d8c6baed29471ef28fe183be7ca62750ae
Deepanshu Verma <deepanshu@example.com>
2026-07-14 19:31:32 +0530

    feat: design playlist schema

---
+++ backend/models/Playlist.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const playlistSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    songs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Song',
+      }
+    ]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Playlist = mongoose.model('Playlist', playlistSchema);
+export default Playlist;
